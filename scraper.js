8e40a15f4a2b232ee4f1ecfb2f465d9e40e0e6f2
Chetan Gupta <cg439@cornell.edu>
2014-11-28 18:02:22 -0500

    Scrapes product descriptions, added interface

--- scraper.js
+++ scraper.js
@@ -1,72 +1,150 @@
 var jsdom = require('jsdom');
+//first argument is the mode of scraping, value inputted should be either "-p" or "-r" for product scraping or review scraping respectively
+//second argument must be Amazon Product ID
+//third argument is the first page to begin scraping from, a numerical number is expected > 0 but less than the last page of available reviews
+//fourth argument is last page to scrape up to for the product review, must be greater than begin page value
+//fifth argument is the ranking criteria for ordering pages when scraping, "-d" for date, "-p" for popularity
+//sixth argument is the order in which the criteria is ordered, either "-a" for ascending or "-d" for descending
+var id, mode, page, endPage, sortMethod, sortOrder;
 
-//example ASIN
-//fill in id for the Amazon product
-//page for initial page to begin grabbing reviews from
-//page for last page to grab reviews up to
-//sort is either bySubmissionDate or byPopularity and can be Ascending or Descending
-var id = "B005VEZ58C",
-    page = 96,
-	endPage = 104,
-	sort = "bySubmissionDateDescending";
+function die(message) {
+	console.log(message);
+	throw '';
+}
 
-var scrape = function(page, endPage) {
+function printHelpMessage() {
+	console.log("Expected usage follows following format");
+	console.log("node scraper.js [mode = -r || -h] [Amazon Product ID] [entryPageNumber] [exitPageNumber] [sortCriteria = -d || -p] [sortOrder = -a || -d]");
+	console.log('First argument is the mode of scraping, value inputted should be either "-p" or "-r" for product scraping or review scraping respectively');
+	console.log("Second argument must be Amazon Product ID");
+	console.log("Third argument is the first page to begin scraping reviews from");
+	console.log("Fourth argument is last page to scrape up to for the product review, must be greater than begin page value");
+	console.log('Fifth argument is the ranking criteria for ordering pages when scraping, "-d" for date, "-p" for popularity');
+	console.log('Sixth argument is the order in which the criteria is ordered, either "-a" for ascending or "-d" for descending');
+	die('Incorrect parameters, please try again. Exiting...');
+}
+
+function processInputs() {
+	if (process.argv.length < 4 || process.argv[2] == "-h") {
+		printHelpMessage();
+	}
+	else {
+		mode = process.argv[2];
+		id = process.argv[3];
+		if (mode == "-r") {
+			page = process.argv[4];
+			endPage = process.argv[5];
+			sortMethod = process.argv[6];
+			sortOrder = process.argv[7];
+			if (sortMethod == "-d") {
+				sortMethod = "bySubmissionDate";
+			}
+			else if (sortMethod == "-p"){
+				sortMethod = "byPopularity";
+			}
+			else {
+				printHelpMessage();
+			}
+			if (sortOrder == "-d") {
+				sortOrder = "Descending";
+			}
+			else if (sortOrder == "-a") {
+				sortOrder = "Ascending";
+			}
+			else {
+				printHelpMessage();
+			}
+		}
+		else if (mode != "-p") {
+			printHelpMessage();
+		}
+	}
+};
+
+
+
+
+processInputs();
+
+var	reviewURL = "http://www.amazon.com/product-reviews/" + id + 
+        "/?ie=UTF8&showViewpoints=0&pageNumber=",
+	productURL = "http://www.amazon.com/dp/" + id;
+	
+var scrapeReviews = function(page, endPage) {
 	var temp = "";
 	var num = 1;
     jsdom.env(
-        "http://www.amazon.com/product-reviews/" + id + 
-        "/? ie=UTF8&showViewpoints=0&pageNumber=" + page +
-        "&sortBy="+sort,
+        reviewURL + page +
+        "&sortBy="+sortMethod+sortOrder,
         ["http://code.jquery.com/jquery.js"],
         function (errors, window) {
             var $ = window.jQuery;  
             var productReviews = $('#productReviews');
-			if(productReviews.length > 0){
+			if (productReviews.length > 0) {
 				productReviews.find('td').children('div:lt(10)').each(function() {
 					var starRating = $(this).find('span:first').text();
-					if(starRating != ""){
-					var helpful = $(this).find('span:first').parent().prev().text();
-					var date = $(this).find('span:first').next().find('nobr').text();
-					var title = $(this).find('span:first').next().find('b').text();
-					var reviewerName = $(this).children().next().find('a:first').find('span').text();
+					if (starRating != "") {
+						var helpful = $(this).find('span:first').parent().prev().text();
+						var date = $(this).find('span:first').next().find('nobr').text();
+						var title = $(this).find('span:first').next().find('b').text();
+						var reviewerName = $(this).children().next().find('a:first').find('span').text();
 
-					var reviewBlock = $(this).find("[class='reviewText']").text();
-					var review = $.trim(reviewBlock);
-					$(this).children().remove();
+						var reviewBlock = $(this).find("[class='reviewText']").text();
+						var review = $.trim(reviewBlock);
+						$(this).children().remove();
+						
 					
-				
-				 // console.log('-----------------------');
-				  //console.log('Title: ' + title  + '\nDate: ' + date + '\nHelpfulness:' + $.trim(helpful) + '\nReviewer:' + reviewerName + '\nRating: ' + starRating + '\nReview:' + review + '\n\n');
-				 console.log(review);
-				 // temp += parseReview(title, date, helpful, starRating, review, num, page, endPage);
-				 //temp += parseAverageReview(date, starRating, num, page, endPage);
-				  }
-				  num++;
-            });}
+					//console.log('-----------------------');
+					//console.log('Title: ' + title  + '\nDate: ' + date + '\nHelpfulness:' + $.trim(helpful) + '\nReviewer:' + reviewerName + '\nRating: ' + starRating + '\nReview:' + review + '\n\n');
+					console.log(review);
+					// temp += parseReview(title, date, helpful, starRating, review, num, page, endPage);
+					//temp += parseAverageReview(date, starRating, num, page, endPage);
+					}
+				num++;
+				});
+			}
 			console.log(temp);
             window.close();
         }
     )        
 };
 
+
+var scrapeProductDescription = function() {
+    jsdom.env(
+        productURL,
+        ["http://code.jquery.com/jquery.js"],
+        function (errors, window) {
+            var $ = window.jQuery;  
+            var product = $('#productDescription');
+			var wrapper = product.find("[class='productDescriptionWrapper']");
+			console.log(wrapper.text());
+            window.close();
+        }
+    )        
+};
+
+
 var parseAverageReview = function(date, star, num, page, endPage){
-//begin bracket for review
-var output = "{ ";
+	//begin bracket for review
+	var output = "{ ";
 
-//splits date which is in format 'month day, year' around spaces for easy access to month and year
-var dateSplit = date.split(" ");
+	//splits date which is in format 'month day, year' around spaces for easy access to month and year
+	var dateSplit = date.split(" ");
 
-//outputs the month and year variables in accordance with json
-output += '"month": "' + dateSplit[0] +'", ';
-output += '"year": ' + dateSplit[2] + ', ';
+	//outputs the month and year variables in accordance with json
+	output += '"month": "' + dateSplit[0] +'", ';
+	output += '"year": ' + dateSplit[2] + ', ';
 
-//outputs rating
-output += '"rating": ' + star.substring(0,1);
-if(num == 10 && page == endPage)
-	output +=  ' }]}';
-else
-	output +=  ' } , ';
-return output;
+	//outputs rating
+	output += '"rating": ' + star.substring(0,1);
+	if (num == 10 && page == endPage) {
+		output +=  ' }]}';
+	}
+	else {
+		output +=  ' } , ';
+	}
+	return output;
 }
 
 
@@ -82,15 +160,17 @@ var parseReview = function (title, date, helpful, rating, review, num, page, end
 	output += '"impact": ' + impact + ', "helpful": ' + helpfulRatio + ', ';
 	output += '"rating": ' + rating.substring(0,1);
 	//output += '"review": "' + review.replace('"', '');
-	if(num == 10 && page == endPage)
+	if (num == 10 && page == endPage) {
 		output +=  ' }]}';
-	else
+	}
+	else {
 		output +=  ' } , ';
+	}
 	return output;
 }
 
 //Text for parsing reviews already existing in a separate file
-	var parse = function(str) {
+var parse = function(str) {
 	if(str.indexOf("Title:") != -1)
 		return '"title": "' + str.trim(str.substring(6))+'", ';
 	else if(str.indexOf("Date:") != -1)
@@ -118,9 +198,13 @@ var parseReview = function (title, date, helpful, rating, review, num, page, end
 
 //insert initial text for the block
 //console.log('{ "reviews": ['); 
-
-//loop until all pages have been parsed
-while(page <= endPage){ 
-	scrape(page, endPage);
-	page++;
+if (mode == "-r") {
+	//loop until all pages have been parsed
+	while(page <= endPage){ 
+		scrapeReviews(page, endPage);
+		page++;
+	}	
+}
+else {
+	scrapeProductDescription();
 }
